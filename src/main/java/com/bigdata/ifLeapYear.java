f147d0c1de854790df99e6cd4cabae0f6bc1565e
yijunchen1996 <2533158235@qq.com>
2020-12-07 13:56:52 +0800

    MYudf

---
+++ src/main/java/com/bigdata/ifLeapYear.java
@@ -0,0 +1,22 @@
+package com.bigdata;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+public class ifLeapYear extends UDF {
+    String year=null;
+    public String evaluate(final String hiredate) {
+        if (hiredate.length()>0) {
+            String[] dates = hiredate.split("-");
+            year=dates[0];
+            int year2int=Integer.parseInt(year);
+            if(year2int%4==0&&year2int%100!=0){
+                return "leap year";
+            }else if(year2int%400==0){
+                return "leap year";
+            }else{
+                return "noleap year";
+            }
+        }
+        return null;
+    }
+}
